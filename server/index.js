290d68ab9f227aceffe5618082e6b655e166189c
impushkarev <55238622+impushkarev@users.noreply.github.com>
2020-05-22 21:27:07 +0300

    2205Version1.2
    Server package.json change

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -11,8 +12,15 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
-
 app.use('/', require('./routes/router'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '/../', 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/../', 'client', 'build', 'index.html'))
+  })
+}
 
 const start = async () => {
   try {
